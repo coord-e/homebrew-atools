c2aabdbfa303305316771907b04a961756cf0c03
coord.e <me@coord-e.com>
2018-04-28 01:39:36 +0900

    Initial commit with formula

---
+++ atools.rb
@@ -0,0 +1,17 @@
+require "formula"
+
+class ATools < Formula
+  homepage "https://github.com/coord-e/atools"
+  version 'v1.0.0'
+
+  url "https://github.com/coord-e/atools/archive/v1.0.0.tar.gz"
+  sha256 "0e0bcd378d8c8bf1ecc8c92fc398c9e61ebb38804ce6b8a1623c2f75a78f0c57"
+
+  def install
+    system "./install.sh", prefix
+  end
+
+  def caveats
+    "ATools"
+  end
+end
