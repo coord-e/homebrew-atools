f6d7f8780ef5a28b102883edf229c49fcfa10fcb
Cj-bc <cj-bc@y-modify.org>
2018-04-28 13:42:36 +0900

    Add: dependencies

--- atools.rb
+++ atools.rb
@@ -7,6 +7,13 @@ class Atools < Formula
   url "https://github.com/coord-e/atools/archive/v1.0.0.tar.gz"
   sha256 "0e0bcd378d8c8bf1ecc8c92fc398c9e61ebb38804ce6b8a1623c2f75a78f0c57"
 
+  depends_on "bash-completion" 
+  depends_on "w3m"
+  depends_on "tmux" => :optional
+  depends_on "fzf" => :optional
+  depends_on "peco" => :optional
+  
+
   def install
     system "./install.sh", prefix
   end
